102a79983972ac5ce2160db7f0932ecf92c050e0
Juan SR <jfrancisco1717@gmail.com>
2025-03-03 02:30:27 -0300

    feat(api): create api project

---
+++ apps/pokemon-tcg-api-e2e/jest.config.ts
@@ -0,0 +1,18 @@
+export default {
+  displayName: 'pokemon-tcg-api-e2e',
+  preset: '../../jest.preset.js',
+  globalSetup: '<rootDir>/src/support/global-setup.ts',
+  globalTeardown: '<rootDir>/src/support/global-teardown.ts',
+  setupFiles: ['<rootDir>/src/support/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/pokemon-tcg-api-e2e',
+};
